6ff666165bc8606d551f50f5eda69b6de5e60679
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-16 18:57:22 -0700

    display correct msg for how many spots remaining

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,21 @@ export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0
-  })
+  });
+  const formatSpots = function (num) {
+    switch (num) {
+      case 0:
+        return "no spots remaining";
+      case 1:
+        return "1 spot remaining";
+      default:
+        return `${num} spots remaining`;
+    }
+  }
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots}</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
