c1ce035d7e182d3fa343edb3b9170d7431fb6480
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-17 15:52:50 -0700

    Implement Show component, add story

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import Button from "components/Button";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 import "components/Appointment/index";
@@ -146,4 +147,12 @@ storiesOf("Appointment", module)
     />
   )
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={event => action("onAdd")} />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />
+  )
+
