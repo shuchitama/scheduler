f54d9bfd3d94f5e01189a3826c967d674d28c94c
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-17 16:21:20 -0700

    add more stories for Status and Error

--- stories/index.js
+++ stories/index.js
@@ -166,10 +166,17 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   )
-  .add("Status", () => <Status message="Deleting" />)
-  .add("Error", () =>
+  .add("Deleting", () => <Status message="Deleting" />)
+  .add("Saving", () => <Status message="Saving" />)
+  .add("Error Deleting", () =>
     <Error
       message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  )
+  .add("Error Saving", () =>
+    <Error
+      message="Could not save appointment."
       onClose={action("onClose")}
     />
   )
