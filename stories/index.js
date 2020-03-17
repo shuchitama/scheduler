352a043fc5b62b15d29e636da896b528921eab9a
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-17 16:15:04 -0700

    Implement Error component, add story

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 
 import "components/Appointment/index";
@@ -166,3 +167,9 @@ storiesOf("Appointment", module)
     />
   )
   .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () =>
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  )
