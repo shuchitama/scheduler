dc2f0f2644632144626289f99be822ee5284cc9c
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-17 19:36:09 -0700

    conditionally render Show or Empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 import './styles.scss';
 
-export default function Appointment() {
+export default function Appointment(props) {
+
   return (
     <article className="appointment">
-
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
     </article>
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -197,3 +197,9 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   )
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
