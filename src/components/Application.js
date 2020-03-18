2c73d4af3ef56bbc005f46e0035b4f26871ad284
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-18 14:32:58 -0700

    import axios, useEffect; remove hardcoded days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import DayList from "./DayList";
 import "components/Appointment";
 
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = [
   {
