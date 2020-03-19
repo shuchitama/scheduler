fb5df27faf5834861f67416915d619413074a236
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-19 13:12:13 -0700

    create and test custom hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+
+export default function useVisualMode(initMode) {
+  const [mode, setMode] = useState(initMode);
+  const [history, setHistory] = useState([initMode])
+  const transition = function (newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    setMode(newMode);
+    setHistory([...history, newMode]);
+  }
+  const back = function () {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1])
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  }
+}
