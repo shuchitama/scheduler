d47f315fef613c95713294234e301ca7d7ff7b81
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-18 19:43:37 -0700

    implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export default function getAppointmentsForDay(state, day) {
+const getAppointmentsForDay = function (state, day) {
   const results = [];
   const matchingDay = state.days.filter(item => item.name === day);
   if (matchingDay.length === 0) return results;
@@ -12,4 +12,14 @@ export default function getAppointmentsForDay(state, day) {
     }
   }
   return results;
-}
+};
+
+const getInterview = function (state, interview) {
+  if (interview === null) return null;
+  const interviewerID = interview.interviewer;
+  const interviewerDetails = state.interviewers[interviewerID];
+  return { ...interview, interviewer: interviewerDetails }
+};
+
+
+export { getAppointmentsForDay, getInterview }
