1075de2f386c21abac34a3d24c7fc8f820f5a99c
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-20 19:02:02 -0700

    add save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,6 +16,15 @@ export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  const save = function (name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+  }
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -33,7 +42,7 @@ export default function Appointment(props) {
           name={props.name}
           interviewers={props.interviewers}
           interviewer={props.interviewer}
-          // onSave={action("onSave")}
+          onSave={save}
           onCancel={back}
         />
       )}
