c71cd908ce22330704716f9c277dc6c485757ac9
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-23 16:35:32 -0700

    add data-testid attribute to input

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,37 +9,50 @@ export default function Form(props) {
   const reset = () => {
     setName("");
     setInterviewer(null);
-  }
+  };
   const cancel = () => {
     reset();
     onCancel();
-  }
+  };
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-          <input
-            className="appointment__create-input text--semi-bold"
-            value={name}
-            type="text"
-            placeholder="Enter Student Name"
-            onChange={(event) => setName(event.target.value)}
-          /*
-            This must be a controlled component
-          */
-          />
+          {
+            <input
+              className="appointment__create-input text--semi-bold"
+              name="name"
+              type="text"
+              placeholder="Enter Student Name"
+              value={name}
+              onChange={event => {
+                setName(event.target.value);
+              }}
+              data-testid="student-name-input"
+            />
+          }
         </form>
         <InterviewerList
           interviewers={interviewers}
           interviewer={interviewer}
-          setInterviewer={setInterviewer} />
+          setInterviewer={setInterviewer}
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => { onSave(name, interviewer) }} confirm>Save</Button>
+          <Button onClick={cancel} danger>
+            Cancel
+          </Button>
+          <Button
+            onClick={() => {
+              onSave(name, interviewer);
+            }}
+            confirm
+          >
+            Save
+          </Button>
         </section>
       </section>
     </main>
-  )
+  );
 }
