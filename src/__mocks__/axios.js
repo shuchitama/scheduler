a1f7db1453749cebc47bdf3cb64ac555ced56014
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-24 20:17:25 -0700

    add mock delete request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -93,5 +93,20 @@ export default {
         statusText: "No Content"
       });
     }
+  }),
+
+  delete: jest.fn(url => {
+    if (url === "/api/appointments/1") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content"
+      });
+    }
+    if (url === "/api/appointments/2") {
+      return Promise.resolve({
+        status: 304,
+        statusText: "Not Modified"
+      });
+    }
   })
 };
