a9c22e430680e6e93c134b5fecf9d3aa59acddab
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-03-24 19:24:14 -0700

    add data-testid="day"

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,12 +4,12 @@ import classnames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const { name, spots, selected, setDay } = props
+  const { name, spots, selected, setDay } = props;
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": selected,
     "day-list__item--full": spots === 0
   });
-  const formatSpots = function (num) {
+  const formatSpots = function(num) {
     switch (num) {
       case 0:
         return "no spots remaining";
@@ -18,9 +18,9 @@ export default function DayListItem(props) {
       default:
         return `${num} spots remaining`;
     }
-  }
+  };
   return (
-    <li className={dayClass} onClick={() => setDay(name)}>
+    <li className={dayClass} data-testid="day" onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
